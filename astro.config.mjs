00d59dc62514ade9a4d9986e6617ff345ff05ab0
@zimeg <zim@o526.net>
2024-08-01 22:26:03 -0700

    build(mdx): include mdx in the astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://o526.net",
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   markdown: {
     remarkRehype: {
       footnoteLabel: "footnotes and further reading",
